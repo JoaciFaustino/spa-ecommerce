b2e47499c934cede8b7714540136df2fa5f10a46
Joaci Faustino <joacifaustino@gmail.com>
2024-09-14 15:23:24 -0300

    [fix]: fixed the bug where the CakeCard didn't work on mobile.

--- src/components/CakeCard/CakeCard.tsx
+++ src/components/CakeCard/CakeCard.tsx
@@ -1,7 +1,9 @@
+"use client";
 import Image from "next/image";
 import styles from "./CakeCard.module.scss";
 import { CgShoppingCart } from "react-icons/cg";
 import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
 
 type Props = {
   cakeId: string;
@@ -12,8 +14,38 @@ type Props = {
 };
 
 function CakeCard({ cakeId, nameCake, typeCake, imageCake, priceCake }: Props) {
+  const infoCardRef = useRef<HTMLDivElement | null>(null);
+
+  // useEffect(() => {
+  //   document.addEventListener("mousedown", handleClickOutsideCakeCard);
+
+  //   return () => {
+  //     document.removeEventListener("mousedown", handleClickOutsideCakeCard);
+  //   };
+  // }, []);
+
+  // const handleClickOutsideCakeCard: EventListener = (event) => {
+  //   const { current } = infoCardRef;
+
+  //   if (current && !current.contains(event.target as Node)) {
+  //     current?.classList.remove(styles.infoOpen);
+  //   }
+  // };
+
+  // const handleClick = () => {
+  //   infoCardRef.current?.classList.toggle(styles.infoOpen);
+  // };
+
+  const handleFocus = () => {
+    infoCardRef.current?.classList.add(styles.infoOpen);
+  };
+
+  const handleBlur = () => {
+    infoCardRef.current?.classList.remove(styles.infoOpen);
+  };
+
   return (
-    <div className={styles.card}>
+    <button className={styles.card} onFocus={handleFocus} onBlur={handleBlur}>
       <div className={styles.divImg}>
         <Image
           src={imageCake}
@@ -23,27 +55,29 @@ function CakeCard({ cakeId, nameCake, typeCake, imageCake, priceCake }: Props) {
           style={{ objectFit: "contain" }}
         />
       </div>
+
       <div className={styles.divText}>
         <div className={styles.divTypeCake}>
           <p className="textBig">{nameCake}</p>
         </div>
         <h4 className={styles.price}>{priceCake}</h4>
       </div>
-      <div className={styles.hoverInfo}>
+
+      <div className={styles.info} ref={infoCardRef}>
         <h4>{nameCake}</h4>
         <p className="textBig">{nameCake}</p>
         <Link href={`/cake/${cakeId}`}>
-          <button className={styles.btnCart}>
+          <div className={styles.btnCart}>
             <CgShoppingCart
               style={{
                 color: "var(--primary-color)",
                 fontSize: "1.5rem"
               }}
             />
-          </button>
+          </div>
         </Link>
       </div>
-    </div>
+    </button>
   );
 }
 
