576220790c34e95d995f82dfd131d091bb3044c9
Joaci Faustino <joacifaustino@gmail.com>
2024-11-16 16:02:21 -0300

    [feat]: added order verification logic and displays custom message if no orders exist in the purchases page

--- src/app/purchases/[userId]/page.tsx
+++ src/app/purchases/[userId]/page.tsx
@@ -7,6 +7,8 @@ import styles from "@/styles/pages/Purchases.module.scss";
 import OrderCard from "@/components/OrderCard/OrderCard";
 import LoadNextOrders from "./components/LoadNextOrders/LoadNextOrders";
 import ScrollTop from "@/components/ScrollTop/ScrollTop";
+import Link from "next/link";
+import { BsCartX } from "react-icons/bs";
 
 type Props = {
   params: { userId: string };
@@ -25,14 +27,37 @@ async function PurchasesPage({ params: { userId } }: Props) {
 
     const { nextUrl, orders } = await getAllUserOrders(limit, page, userId);
 
+    const thereIsOrders = orders.length > 0;
+
     return (
       <section className={styles.mainSection}>
         <div className={`${styles.wrapper} wrapper`}>
-          {orders.map((order) => (
-            <OrderCard order={order} key={order._id} />
-          ))}
+          {!thereIsOrders && (
+            <div className={styles.divText}>
+              <h4>Você ainda não fez nenhum pedido!</h4>
 
-          <LoadNextOrders nextUrl={nextUrl} />
+              <BsCartX
+                style={{
+                  color: "var(--color-text-paragraph)",
+                  fontSize: "12rem"
+                }}
+              />
+
+              <Link href="/menu?sortBy=popularidade">
+                <button>Ir para o menu</button>
+              </Link>
+            </div>
+          )}
+
+          {thereIsOrders && (
+            <>
+              {orders.map((order) => (
+                <OrderCard order={order} key={order._id} />
+              ))}
+            </>
+          )}
+
+          {thereIsOrders && <LoadNextOrders nextUrl={nextUrl} />}
         </div>
 
         <ScrollTop />
@@ -41,7 +66,7 @@ async function PurchasesPage({ params: { userId } }: Props) {
   } catch (error) {
     if (error instanceof CustomError && error.status === 401) {
       //tem que fazer a pagina global de 401
-
+      //
       redirect(`/`);
     }
 
