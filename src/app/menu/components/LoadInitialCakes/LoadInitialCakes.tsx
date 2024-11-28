f02532adbbbb54afdf610c3287feb34a860389f6
Joaci Faustino <joacifaustino@gmail.com>
2024-11-28 11:55:20 -0300

    [refactor]: separated the logic for fetching initial cakes from the LoadInitialCakes component into a utility function in the utils folder, improving code organization and reusability

--- src/app/menu/components/LoadInitialCakes/LoadInitialCakes.tsx
+++ src/app/menu/components/LoadInitialCakes/LoadInitialCakes.tsx
@@ -3,67 +3,34 @@ import { formatPriceNumber } from "@/utils/formatPrice";
 import LoadNextCakes from "../LoadNextCakes/LoadNextCakes";
 import CakeCard from "@/components/CakeCard/CakeCard";
 import { CakeQueryParams } from "@/@types/QueryParams";
-import { sortByApiOptions } from "@/@types/SortBy";
-import { getAllCakes, getFirstPageCakesCached } from "@/services/cakes";
 import styles from "./LoadInitialCakes.module.scss";
 import { CustomError } from "@/utils/customError";
-import { ICake } from "@/@types/Cake";
 import RefreshPageButton from "@/components/RefreshPageButton/RefreshPageButton";
-
-const SORT_BY_API_OPTIONS: sortByApiOptions = {
-  popularidade: "popularity",
-  novos: "latest",
-  "preço: do maior para o menor": "price_high_to_low",
-  "preço: do menor para o maior": "price_low_to_high"
-};
-
-const splitQueryParam = (queryParam: undefined | string | string[]) => {
-  if (!queryParam) {
-    return;
-  }
-
-  return typeof queryParam === "string" ? queryParam.split(",") : queryParam;
-};
+import { getInitialCakes } from "@/utils/getInitialCakes";
 
 type Props = { searchParams: CakeQueryParams };
 
 async function LoadInitialCakes({ searchParams }: Props) {
-  const { sortBy, category, filling, frosting, search, size, type } =
-    searchParams;
-
-  const sortByLastValue = Array.isArray(sortBy)
-    ? sortBy[sortBy.length - 1]
-    : sortBy;
-
   try {
-    const shouldGetCachedCakes =
-      (!sortBy || sortBy === "popularidade") &&
-      (!category || category.length === 0) &&
-      (!filling || filling.length === 0) &&
-      (!frosting || frosting.length === 0) &&
-      (!search || search.length === 0) &&
-      (!size || size.length === 0) &&
-      (!type || type.length === 0);
+    const { cakes, nextUrl } = await getInitialCakes(searchParams);
 
-    if (shouldGetCachedCakes) {
-      const { cakes, nextUrl } = await getFirstPageCakesCached();
+    return (
+      <>
+        {cakes.map((cake) => (
+          <CakeCard
+            key={cake._id}
+            id={cake._id}
+            name={cake.name}
+            imageUrl={cake.imageUrl}
+            price={formatPriceNumber(cake.totalPricing)}
+            categories={cake.categories}
+            customizableParts={cake.customizableParts}
+          />
+        ))}
 
-      return <CakeList cakes={cakes} nextUrl={nextUrl} />;
-    }
-
-    const { cakes, nextUrl } = await getAllCakes({
-      limit: "12",
-      page: "1",
-      search,
-      type: splitQueryParam(type),
-      category: splitQueryParam(category),
-      size: splitQueryParam(size),
-      filling: splitQueryParam(filling),
-      frosting: splitQueryParam(frosting),
-      sortBy: SORT_BY_API_OPTIONS[sortByLastValue || ""] ?? undefined
-    });
-
-    return <CakeList cakes={cakes} nextUrl={nextUrl} />;
+        <LoadNextCakes nextUrl={nextUrl || undefined} />
+      </>
+    );
   } catch (error: any) {
     const statusCode = error instanceof CustomError ? error.status : 500;
 
@@ -79,31 +46,6 @@ async function LoadInitialCakes({ searchParams }: Props) {
       </div>
     );
   }
-}
-
-type CakeListProps = {
-  cakes: ICake[];
-  nextUrl: string | null;
-};
-
-function CakeList({ cakes, nextUrl }: CakeListProps) {
-  return (
-    <>
-      {cakes.map((cake) => (
-        <CakeCard
-          key={cake._id}
-          id={cake._id}
-          name={cake.name}
-          imageUrl={cake.imageUrl}
-          price={formatPriceNumber(cake.totalPricing)}
-          categories={cake.categories}
-          customizableParts={cake.customizableParts}
-        />
-      ))}
-
-      <LoadNextCakes nextUrl={nextUrl || undefined} />
-    </>
-  );
 }
 
 export default LoadInitialCakes;
---
+++ src/utils/getInitialCakes.ts
@@ -0,0 +1,60 @@
+import { CakeQueryParams } from "@/@types/QueryParams";
+import { sortByApiOptions } from "@/@types/SortBy";
+import { getAllCakes, getFirstPageCakesCached } from "@/services/cakes";
+
+const SORT_BY_API_OPTIONS: sortByApiOptions = {
+  popularidade: "popularity",
+  novos: "latest",
+  "preço: do maior para o menor": "price_high_to_low",
+  "preço: do menor para o maior": "price_low_to_high"
+};
+
+const splitQueryParam = (queryParam: undefined | string | string[]) => {
+  if (!queryParam) {
+    return;
+  }
+
+  return typeof queryParam === "string" ? queryParam.split(",") : queryParam;
+};
+
+export const getInitialCakes = async (searchParams: CakeQueryParams) => {
+  const { sortBy, category, filling, frosting, search, size, type } =
+    searchParams;
+
+  const sortByLastValue = Array.isArray(sortBy)
+    ? sortBy[sortBy.length - 1]
+    : sortBy;
+
+  try {
+    const shouldGetCachedCakes =
+      (!sortBy || sortBy === "popularidade") &&
+      (!category || category.length === 0) &&
+      (!filling || filling.length === 0) &&
+      (!frosting || frosting.length === 0) &&
+      (!search || search.length === 0) &&
+      (!size || size.length === 0) &&
+      (!type || type.length === 0);
+
+    if (shouldGetCachedCakes) {
+      const { cakes, nextUrl } = await getFirstPageCakesCached();
+
+      return { cakes, nextUrl };
+    }
+
+    const { cakes, nextUrl } = await getAllCakes({
+      limit: "12",
+      page: "1",
+      search,
+      type: splitQueryParam(type),
+      category: splitQueryParam(category),
+      size: splitQueryParam(size),
+      filling: splitQueryParam(filling),
+      frosting: splitQueryParam(frosting),
+      sortBy: SORT_BY_API_OPTIONS[sortByLastValue || ""] ?? undefined
+    });
+
+    return { cakes, nextUrl };
+  } catch (error) {
+    throw error;
+  }
+};
