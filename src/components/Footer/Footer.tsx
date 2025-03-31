3a90398d428365d90c76bb9850ed29a96d03e3ae
Joaci Faustino <joacifaustino@gmail.com>
2025-03-31 14:16:49 -0300

    [feat]: replaced the Facebook link with the GitHub link in the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { BsFacebook, BsInstagram, BsWhatsapp } from "react-icons/bs";
+import { BsGithub, BsInstagram, BsWhatsapp } from "react-icons/bs";
 import styles from "./Footer.module.scss";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -14,14 +14,19 @@ function Footer() {
         <footer className={styles.footer}>
           <div className={styles.wrapper}>
             <div className={styles.socialMedias}>
-              <Link href="https://instagram.com/joacifaustino" target="_blank">
+              <Link href="https://github.com/JoaciFaustino" target="_blank">
+                <div className={styles.divIcon}>
+                  <BsGithub
+                    className={styles.icon}
+                    style={{ color: "#ffffff", fontSize: "1.5rem" }}
+                  />
+                </div>
+              </Link>
+              <Link href="https://github.com/JoaciFaustino" target="_blank">
                 <div className={styles.divIcon}>
                   <BsInstagram
                     className={styles.icon}
-                    style={{
-                      color: "#ffffff",
-                      fontSize: "1.5rem"
-                    }}
+                    style={{ color: "#ffffff", fontSize: "1.5rem" }}
                   />
                 </div>
               </Link>
@@ -29,21 +34,7 @@ function Footer() {
                 <div className={styles.divIcon}>
                   <BsWhatsapp
                     className={styles.icon}
-                    style={{
-                      color: "#ffffff",
-                      fontSize: "1.5rem"
-                    }}
-                  />
-                </div>
-              </Link>
-              <Link href="https://facebook.com/joaci.faustino/" target="_blank">
-                <div className={styles.divIcon}>
-                  <BsFacebook
-                    className={styles.icon}
-                    style={{
-                      color: "#ffffff",
-                      fontSize: "1.5rem"
-                    }}
+                    style={{ color: "#ffffff", fontSize: "1.5rem" }}
                   />
                 </div>
               </Link>
